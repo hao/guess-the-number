5e9986bab244ff5606a7799d0682f7b69c2f5868
hao <hao@iteaha.us>
2016-04-29 16:49:07 -0400

    Add possible optimization for getting the bounds.

--- js/components/prompt/prompt.react.js
+++ js/components/prompt/prompt.react.js
@@ -2,10 +2,8 @@ var React = require('react');
 var promptRT = require('./prompt.rt');
 var guessTheNumberStore = require('../../stores/guess-the-number-store');
 
-var bounds = guessTheNumberStore.getBounds();
-
 var Prompt = React.createClass({
-  bounds: bounds,
+  bounds: guessTheNumberStore.getBounds(),
   render: promptRT
 });
 
