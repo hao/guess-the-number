f8a73dffade0f394b6733370dd2a5ede2dde7d9c
hao <hao@iteaha.us>
2016-04-30 04:04:07 -0400

    Remove the outdated settings class.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var GuessTheNumberApp = require('./components/guess-the-number-app.react');
-var GuessTheNumberSettings = require('./components/guess-the-number-settings.react');
 
 ReactDOM.render(
   <GuessTheNumberApp />,
