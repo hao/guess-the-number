42647b89a2ddbbaff519e56eb45b1c4caa352ec1
hao <hao@iteaha.us>
2016-04-29 22:33:56 -0400

    Add http-server and react-router.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 var React = require('react');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+var Link = require('react-router').Link;
+var IndexLink = require('react-router').IndexLink;
+var browserHistory = require('react-router').browserHistory;
 
 var GuessTheNumberApp = require('./components/guess-the-number-app.react');
 
 React.render(
-  <GuessTheNumberApp />,
-  document.getElementById('guess-the-number-app')
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={GuessTheNumberApp}>
+      </Route>
+    </Router>
+  ), document.getElementById('guess-the-number-app')
 );
