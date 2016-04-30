ef3f5b1bd9316d90c63da03130d59f4bfdf54a4f
hao <hao@iteaha.us>
2016-04-29 20:46:42 -0400

    Capitalize variable names per common practice.

--- js/stores/__tests__/guess-the-number-store-test.js
+++ js/stores/__tests__/guess-the-number-store-test.js
@@ -2,10 +2,10 @@ jest.dontMock('../../constants/guess-the-number-constants');
 jest.dontMock('../guess-the-number-store');
 jest.dontMock('object-assign');
 
-describe('guessTheNumberStore', function () {
-  var guessTheNumberConstants = require('../../constants/guess-the-number-constants');
+describe('GuessTheNumberStore', function () {
+  var GuessTheNumberConstants = require('../../constants/guess-the-number-constants');
   var AppDispatcher;
-  var guessTheNumberStore;
+  var GuessTheNumberStore;
   var callback;
 
   // mock values
@@ -14,23 +14,23 @@ describe('guessTheNumberStore', function () {
 
   // mock actions
   var mockActionSetLowerBound = {
-    actionType: guessTheNumberConstants.ACTION_BOUNDS_SET_LOWER,
+    actionType: GuessTheNumberConstants.ACTION_BOUNDS_SET_LOWER,
     value: mockLowerBound
   };
   var mockActionSetUpperBound = {
-    actionType: guessTheNumberConstants.ACTION_BOUNDS_SET_UPPER,
+    actionType: GuessTheNumberConstants.ACTION_BOUNDS_SET_UPPER,
     value: mockUpperBound
   };
   var mockActionResetBounds = {
-    actionType: guessTheNumberConstants.ACTION_BOUNDS_RESET
+    actionType: GuessTheNumberConstants.ACTION_BOUNDS_RESET
   };
   var mockActionGuessNumber = {
-    actionType: guessTheNumberConstants.ACTION_GUESSES_GUESS
+    actionType: GuessTheNumberConstants.ACTION_GUESSES_GUESS
   };
 
   beforeEach(function () {
     AppDispatcher = require('../../dispatcher/AppDispatcher');
-    guessTheNumberStore = require('../guess-the-number-store');
+    GuessTheNumberStore = require('../guess-the-number-store');
     callback = AppDispatcher.register.mock.calls[0][0];
   });
 
@@ -39,43 +39,43 @@ describe('guessTheNumberStore', function () {
   });
 
   it('should initialize the lower bound', function () {
-    var bounds = guessTheNumberStore.getBounds();
+    var bounds = GuessTheNumberStore.getBounds();
     expect(bounds.lower).toEqual(1);
   });
 
   it('should initialize the upper bound', function () {
-    var bounds = guessTheNumberStore.getBounds();
+    var bounds = GuessTheNumberStore.getBounds();
     expect(bounds.upper).toEqual(10);
   });
 
   it('should initialize the random number', function () {
-    var number = guessTheNumberStore.getNumberToGuess();
+    var number = GuessTheNumberStore.getNumberToGuess();
     expect(number).toBeDefined();
     expect(number).toNotEqual(null);
   });
 
   it('should set the lower bound', function () {
     callback(mockActionSetLowerBound);
-    var bounds = guessTheNumberStore.getBounds();
+    var bounds = GuessTheNumberStore.getBounds();
     expect(bounds.lower).toEqual(mockLowerBound);
   });
 
   it('should set the upper bound', function () {
     callback(mockActionSetUpperBound);
-    var bounds = guessTheNumberStore.getBounds();
+    var bounds = GuessTheNumberStore.getBounds();
     expect(bounds.upper).toEqual(mockUpperBound);
   });
 
   it('should reset the lower bound', function () {
     callback(mockActionResetBounds);
-    var bounds = guessTheNumberStore.getBounds();
-    expect(bounds.lower).toEqual(guessTheNumberConstants.BOUNDS_LOWER_DEFAULT);
+    var bounds = GuessTheNumberStore.getBounds();
+    expect(bounds.lower).toEqual(GuessTheNumberConstants.BOUNDS_LOWER_DEFAULT);
   });
 
   it('should reset the upper bound', function () {
     callback(mockActionResetBounds);
-    var bounds = guessTheNumberStore.getBounds();
-    expect(bounds.upper).toEqual(guessTheNumberConstants.BOUNDS_UPPER_DEFAULT);
+    var bounds = GuessTheNumberStore.getBounds();
+    expect(bounds.upper).toEqual(GuessTheNumberConstants.BOUNDS_UPPER_DEFAULT);
   });
 
   it('should allow guessing the number', function () {
@@ -84,18 +84,18 @@ describe('guessTheNumberStore', function () {
     var firstGuessCorrect;
     var secondGuessCorrect;
 
-    actionSetUpperBound.value = guessTheNumberConstants.BOUNDS_LOWER_DEFAULT + 1;
+    actionSetUpperBound.value = GuessTheNumberConstants.BOUNDS_LOWER_DEFAULT + 1;
 
     callback(mockActionResetBounds);
     callback(actionSetUpperBound);
 
-    actionGuessNumber.value = guessTheNumberConstants.BOUNDS_LOWER_DEFAULT;
+    actionGuessNumber.value = GuessTheNumberConstants.BOUNDS_LOWER_DEFAULT;
     callback(actionGuessNumber);
-    firstGuessCorrect = guessTheNumberStore.isLastGuessCorrect();
+    firstGuessCorrect = GuessTheNumberStore.isLastGuessCorrect();
 
-    actionGuessNumber.value = guessTheNumberConstants.BOUNDS_LOWER_DEFAULT + 1;
+    actionGuessNumber.value = GuessTheNumberConstants.BOUNDS_LOWER_DEFAULT + 1;
     callback(actionGuessNumber);
-    secondGuessCorrect = guessTheNumberStore.isLastGuessCorrect();
+    secondGuessCorrect = GuessTheNumberStore.isLastGuessCorrect();
 
     expect(firstGuessCorrect || secondGuessCorrect).toBe(true);
   });
